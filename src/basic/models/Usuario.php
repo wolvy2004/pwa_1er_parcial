681221c9c3c3bbcb76425b154e6c1653502d9732
wolvy2004 <eduardoguerradg@gmail.com>
2021-11-24 00:49:19 -0300

    2) Crear los archivos de modelo

---
+++ src/basic/models/Usuario.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "usuario".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $nombre
+ * @property string $apellido
+ */
+class Usuario extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'usuario';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'username', 'nombre', 'apellido'], 'required'],
+            [['id'], 'integer'],
+            [['username'], 'string', 'max' => 50],
+            [['nombre', 'apellido'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'nombre' => 'Nombre',
+            'apellido' => 'Apellido',
+        ];
+    }
+}
