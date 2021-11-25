bd8585d12fcc44bc4b895b9221ed79cd6686da27
wolvy2004 <eduardoguerradg@gmail.com>
2021-11-24 23:07:49 -0300

    6) Implementar la clase IdentityInterface

--- src/basic/models/Usuario.php
+++ src/basic/models/Usuario.php
@@ -15,7 +15,7 @@ use Yii;
  * @property string $accessToken
  * @property string $authKey
  */
-class Usuario extends \yii\db\ActiveRecord
+class Usuario extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -53,5 +53,67 @@ class Usuario extends \yii\db\ActiveRecord
             'accessToken' => 'Access Token',
             'authKey' => 'Auth Key',
         ];
+    }
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return self::findOne(["accessToken"=> $token]);
+        }
+
+        
+    
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return self::findOne(["username"=>$username]);
+        }
+
+      
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return password_verify($password, $this->password);
     }
 }
